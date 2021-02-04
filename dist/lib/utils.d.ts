3e7609ed374b42d680d33ba4673c4d8f1c7e84cf
yon.cuadrado <yon.cuadrado@gmail.com>
2021-02-04 22:35:04 +0100

    package fixed

--- dist/lib/utils.d.ts
+++
@@ -1,8 +0,0 @@
-import { ErrorMessage, Step } from './models';
-import { HookStats, TestStats } from '@wdio/reporter';
-export default class Utils {
-    getStepKeywords(language: string): string[];
-    getFailedMessage(testObject: TestStats | HookStats): ErrorMessage;
-    containsSteps(steps: Step[], language: string): boolean;
-    keywordStartsWith(title: string, language: string): string | undefined;
-}
